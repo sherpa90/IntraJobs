ef7c2cc515924c15f7902291ea675a60bdd924e9
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-18 02:25:30 -0400

    Solo esteban hace publicaciones

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,29 +1,27 @@
 # app/controllers/job_applications_controller.rb
 class JobApplicationsController < ApplicationController
-    before_action :set_job_offer, only: [:new, :create]
-    
-    def new
-      @job_application = @job_offer.applications.build
-    end
-    
-    def create
-      @job_application = @job_offer.applications.build(job_application_params)
-      
-      if @job_application.save
-        redirect_to @job_offer, notice: "Application submitted successfully."
-      else
-        render :new
-      end
-    end
-    
-    private
-    
-    def set_job_offer
-      @job_offer = JobOffer.find(params[:job_offer_id])
-    end
-    
-    def job_application_params
-      params.require(:job_application).permit(:cover_letter).merge(user_id: current_user.id)
+  before_action :authenticate_user!
+
+  def new
+    @job_offer = JobOffer.find(params[:job_offer_id])
+    @job_application = JobApplication.new
+  end
+
+  def create
+    @job_offer = JobOffer.find(params[:job_offer_id])
+    @job_application = current_user.job_applications.build(job_application_params)
+    @job_application.job_offer = @job_offer
+
+    if @job_application.save
+      redirect_to job_offer_path(@job_offer), notice: "¡Postulación creada exitosamente!"
+    else
+      render :new
     end
   end
-  
+
+  private
+
+  def job_application_params
+    params.require(:job_application).permit(:cover_letter)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   
-  resources :job_offers
+  resources :job_offers do
+    resources :job_applications, only: [:new, :create]
+  end
   
   root "job_offers#index"
 end
